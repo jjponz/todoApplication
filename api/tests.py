aa57c16f4288588f5d636578aaf4b103db10b67e
Juanjo <jjponzva@gmail.com>
2019-04-21 00:20:51 +0200

    Create todo add creational date

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.test import TestCase
 import json
 from rest_framework import status
@@ -20,3 +21,17 @@ class TodoTests(TestCase):
                                content_type='application/json')
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_add_todo_with_creational_date(self):
+        todo = Todo(description="Do the webpage")
+        todo_serializer = TodoSerializer(todo)
+
+        response = client.post(reverse('todo_management'),
+                               data=todo_serializer.data,
+                               content_type='application/json')
+
+        created_todo = Todo(**response.data)
+        self.assertEqual(self.__today(), created_todo.creation_date)
+
+    def __today(self):
+        return datetime.today().date().strftime('%Y-%m-%d')
--- todo/models/todo.py
+++ todo/models/todo.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Todo(models.Model):
     description = models.TextField(blank=False)
+    creation_date = models.DateField(auto_now=True)
 
     class Meta:
         app_label = "todo"
